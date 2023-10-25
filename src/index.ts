2502973e3d199b478966ee3f49cea2d9c658fca3
Aditya Patadia <aditya@patadia.org>
2023-10-26 03:30:13 +0530

    bug fix in urltopath

--- src/index.ts
+++ src/index.ts
@@ -127,7 +127,8 @@ class S3Storage extends StorageBase {
   // Doesn't seem to be documented, but required for using this adapter for other media file types.
   // Seealso: https://github.com/laosb/ghos3/pull/6
   urlToPath(url) {
-    return url;
+    const parsedurl = new URL(url);
+    return parsedurl.pathname;
   }
 
   async save(image: Image, targetDir?: string) {
